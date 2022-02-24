1938e250194bd17ac8b2696d441a2b2535fc80c0
mthsaguiar <matheusaguiardev@gmail.com>
2022-02-23 21:24:02 -0300

    general adjusts and dog harness landing

--- assets/accordion-image.js
+++ assets/accordion-image.js
@@ -1,8 +1,29 @@
 window.addEventListener('load', ()=>{
+    const accordionBtn = document.querySelectorAll(".accordion-image__img-content .accordion");
+    console.log(accordionBtn);
+    accordionBtn.forEach( element=>{
+        element.addEventListener('click', ()=>{
+            element.classList.toggle('active')
+            let elementNext = element.nextElementSibling;
+            if(element.classList.contains('active')){
+                elementNext.style.height = elementNext.scrollHeight + 'px'
+            }else{
+                elementNext.style.height = '0px'
+            }
+        })
+    } )
+})
+
+
+
+/*
+window.addEventListener('load', ()=>{
     let acc2 = document.querySelectorAll(".accordion-image__img-content .accordion");
     let i;
+    
     for (i = 0; i < acc2.length; i++) {
     acc2[i].addEventListener("click", function() {
+        console.log(acc2);
         this.classList.toggle("active");
         document.querySelectorAll(".accordion-image__img-content .accordion").forEach(e=>{
             if(e != this){
@@ -13,6 +34,7 @@ window.addEventListener('load', ()=>{
         })
         let panel2 = this.nextElementSibling;
         let scrollHeight = panel2.scrollHeight;
+        console.log(panel2,panel2.style.height);
         if (panel2.style.height === scrollHeight+'px' || panel2.style.height == 'auto') {
             panel2.style.height = "0";
         } else {
@@ -20,4 +42,4 @@ window.addEventListener('load', ()=>{
         }
     });
     }
-})
+})*/
