1829a7ea309313d3c26a54b1e24cf49847b5ade8
mthsaguiar <matheusaguiardev@gmail.com>
2022-03-01 11:34:32 -0300

    google analytics code

--- assets/google-analytics.js
+++ assets/google-analytics.js
@@ -3,3 +3,9 @@
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-THWL28J');
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-220836846-1');
